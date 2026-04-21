0c19e35dce2d77931782645f5328e1640f2daaca
nass214 <nassimachir100@gmail.com>
2026-04-22 01:37:41 +0200

    Refactor LogEntry class to include additional properties
    Added properties for timestamp, backup name, source and target file paths, file size, and transfer time. Updated class visibility from internal to public.

--- EasySave/EasyLog/LogEntry.cs
+++ EasySave/EasyLog/LogEntry.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace EasyLog
 {
-    internal class LogEntry
+    public class LogEntry
     {
+        public DateTime Timestamp { get; set; }
+
+        public string BackupName { get; set; }
+
+        public string SourceFilePath { get; set; }
+
+        public string TargetFilePath { get; set; }
+
+        public long FileSize { get; set; }
+
+        public long TransferTimeMs { get; set; }
+
+        public LogEntry()
+        {
+            Timestamp = DateTime.UtcNow;
+            BackupName = string.Empty;
+            SourceFilePath = string.Empty;
+            TargetFilePath = string.Empty;
+        }
     }
 }
